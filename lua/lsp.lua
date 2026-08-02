05b04cb86c12ef84f8177c8e15a1aaf23b8eac90
hail0hydra <107790736+hail0hydra@users.noreply.github.com>
2026-08-03 02:36:34 +0530

    Improve support for Java language
    created custom `workspace` path for JDTLS
    modified the `root_markers` as well for better recognition

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -3,166 +3,171 @@
 -- ====================================
 
 local diagnostic_signs = {
-	Error = " ",
-	Warn = " ",
-	Hint = "",
-	Info = "",
+    Error = " ",
+    Warn = " ",
+    Hint = "",
+    Info = "",
 }
 
 vim.diagnostic.config({
-	virtual_text = { prefix = "●", spacing = 4 },
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
-			[vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
-			[vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
-			[vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
-		},
-	},
-	underline = true,
-	update_in_insert = false,
-	severity_sort = true,
-	float = {
-		border = "rounded",
-		source = true,
-		header = "",
-		prefix = "",
-		focusable = false,
-		style = "minimal",
-	},
+    virtual_text = { prefix = "●", spacing = 4 },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+            [vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+            [vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+            [vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+        },
+    },
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+        header = "",
+        prefix = "",
+        focusable = false,
+        style = "minimal",
+    },
 })
 
 do
-	local orig = vim.lsp.util.open_floating_preview
-	function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-		opts = opts or {}
-		opts.border = opts.border or "rounded"
-		return orig(contents, syntax, opts, ...)
-	end
+    local orig = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or "rounded"
+        return orig(contents, syntax, opts, ...)
+    end
 end
 
 local function lsp_on_attach(ev)
-	local client = vim.lsp.get_client_by_id(ev.data.client_id)
-	if not client then
-		return
-	end
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then
+        return
+    end
 
-	local bufnr = ev.buf
-	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	vim.keymap.set("n", "<leader>gd", function()
-		require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
-	end, opts)
+    if client then -- remove colors imposed by LSP. Retain TSInstall colors
+        client.server_capabilities.semanticTokensProvider = nil
+    end
 
-	vim.keymap.set("n", "<leader>gD", vim.lsp.buf.definition, opts)
+    local bufnr = ev.buf
+    local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	vim.keymap.set("n", "<leader>gS", function()
-		vim.cmd("vsplit")
-		vim.lsp.buf.definition()
-	end, opts)
+    vim.keymap.set("n", "<leader>gd", function()
+        require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+    end, opts)
 
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>gD", vim.lsp.buf.definition, opts)
 
-	vim.keymap.set("n", "<leader>D", function()
-		vim.diagnostic.open_float({ scope = "line" })
-	end, opts)
-	vim.keymap.set("n", "<leader>d", function()
-		vim.diagnostic.open_float({ scope = "cursor" })
-	end, opts)
-	vim.keymap.set("n", "<leader>nd", function()
-		vim.diagnostic.jump({ count = 1 })
-	end, opts)
+    vim.keymap.set("n", "<leader>gS", function()
+        vim.cmd("vsplit")
+        vim.lsp.buf.definition()
+    end, opts)
 
-	vim.keymap.set("n", "<leader>pd", function()
-		vim.diagnostic.jump({ count = -1 })
-	end, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>D", function()
+        vim.diagnostic.open_float({ scope = "line" })
+    end, opts)
+    vim.keymap.set("n", "<leader>d", function()
+        vim.diagnostic.open_float({ scope = "cursor" })
+    end, opts)
+    vim.keymap.set("n", "<leader>nd", function()
+        vim.diagnostic.jump({ count = 1 })
+    end, opts)
 
-	vim.keymap.set("n", "<leader>fd", function()
-		require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
-	end, opts)
-	vim.keymap.set("n", "<leader>fr", function()
-		require("fzf-lua").lsp_references()
-	end, opts)
-	vim.keymap.set("n", "<leader>ft", function()
-		require("fzf-lua").lsp_typedefs()
-	end, opts)
-	vim.keymap.set("n", "<leader>fs", function()
-		require("fzf-lua").lsp_document_symbols()
-	end, opts)
-	vim.keymap.set("n", "<leader>fw", function()
-		require("fzf-lua").lsp_workspace_symbols()
-	end, opts)
-	vim.keymap.set("n", "<leader>fi", function()
-		require("fzf-lua").lsp_implementations()
-	end, opts)
+    vim.keymap.set("n", "<leader>pd", function()
+        vim.diagnostic.jump({ count = -1 })
+    end, opts)
 
-	if client:supports_method("textDocument/codeAction", bufnr) then
-		vim.keymap.set("n", "<leader>oi", function()
-			vim.lsp.buf.code_action({
-				context = { only = { "source.organizeImports" }, diagnostics = {} },
-				apply = true,
-				bufnr = bufnr,
-			})
-			vim.defer_fn(function()
-				vim.lsp.buf.format({ bufnr = bufnr })
-			end, 50)
-		end, opts)
-	end
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+    vim.keymap.set("n", "<leader>fd", function()
+        require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+    end, opts)
+    vim.keymap.set("n", "<leader>fr", function()
+        require("fzf-lua").lsp_references()
+    end, opts)
+    vim.keymap.set("n", "<leader>ft", function()
+        require("fzf-lua").lsp_typedefs()
+    end, opts)
+    vim.keymap.set("n", "<leader>fs", function()
+        require("fzf-lua").lsp_document_symbols()
+    end, opts)
+    vim.keymap.set("n", "<leader>fw", function()
+        require("fzf-lua").lsp_workspace_symbols()
+    end, opts)
+    vim.keymap.set("n", "<leader>fi", function()
+        require("fzf-lua").lsp_implementations()
+    end, opts)
+
+    if client:supports_method("textDocument/codeAction", bufnr) then
+        vim.keymap.set("n", "<leader>oi", function()
+            vim.lsp.buf.code_action({
+                context = { only = { "source.organizeImports" }, diagnostics = {} },
+                apply = true,
+                bufnr = bufnr,
+            })
+            vim.defer_fn(function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+            end, 50)
+        end, opts)
+    end
 end
 
 vim.api.nvim_create_autocmd("LspAttach", { group = augroup, callback = lsp_on_attach })
 
 vim.keymap.set("n", "<leader>q", function()
-	vim.diagnostic.setloclist({ open = true })
+    vim.diagnostic.setloclist({ open = true })
 end, { desc = "Open diagnostic list" })
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
 require("blink.cmp").setup({
-	keymap = {
-		preset = "none",
-		["<C-Space>"] = { "show", "hide" },
-		["<CR>"] = { "accept", "fallback" },
-		["<Tab>"] = { "select_next", "fallback" },
-		["<S-Tab>"] = { "select_prev", "fallback" },
-		["<C-j>"] = { "snippet_forward", "fallback" },
-		["<C-k>"] = { "snippet_backward", "fallback" },
-	},
-	appearance = { nerd_font_variant = "mono" },
-	completion = {
-		menu = {
-			auto_show = true,
-		},
-		accept = {
-			auto_brackets = { enabled = true },
-		},
-	},
-	sources = { default = { "lsp", "path", "buffer", "snippets" } },
-	snippets = {
-		expand = function(snippet)
-			require("luasnip").lsp_expand(snippet)
-		end,
-	},
+    keymap = {
+        preset = "none",
+        ["<C-Space>"] = { "show", "hide" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "snippet_forward", "fallback" },
+        ["<C-k>"] = { "snippet_backward", "fallback" },
+    },
+    appearance = { nerd_font_variant = "mono" },
+    completion = {
+        menu = {
+            auto_show = true,
+        },
+        accept = {
+            auto_brackets = { enabled = true },
+        },
+    },
+    sources = { default = { "lsp", "path", "buffer", "snippets" } },
+    snippets = {
+        expand = function(snippet)
+            require("luasnip").lsp_expand(snippet)
+        end,
+    },
 
-	fuzzy = {
-		implementation = "prefer_rust",
-		prebuilt_binaries = { download = true },
-	},
+    fuzzy = {
+        implementation = "prefer_rust",
+        prebuilt_binaries = { download = true },
+    },
 })
 
 vim.lsp.config["*"] = {
-	capabilities = require("blink.cmp").get_lsp_capabilities(),
+    capabilities = require("blink.cmp").get_lsp_capabilities(),
 }
 
 vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {
-			diagnostics = { globals = { "vim" } },
-			telemetry = { enable = false },
-		},
-	},
+    settings = {
+        Lua = {
+            diagnostics = { globals = { "vim" } },
+            telemetry = { enable = false },
+        },
+    },
 })
 vim.lsp.config("pyright", {})
 vim.lsp.config("bashls", {})
@@ -170,16 +175,16 @@ vim.lsp.config("ts_ls", {})
 vim.lsp.config("gopls", {})
 vim.lsp.config("clangd", {})
 vim.lsp.config("html", {
-	filetypes = {
-		"html",
-		"php",
-	},
+    filetypes = {
+        "html",
+        "php",
+    },
 })
 vim.lsp.config("cssls", {
-	filetypes = {
-		"html",
-		"css",
-	},
+    filetypes = {
+        "html",
+        "css",
+    },
 })
 
 vim.lsp.config("kotlin_lsp", {
@@ -192,83 +197,111 @@ vim.lsp.config("kotlin_lsp", {
     },
 })
 
+
+vim.lsp.config("jdtls", {
+    -- create dedicated workspace for each project for jdtls to keep the index file/catalog
+    cmd = {
+        "jdtls",
+        "-data",
+        (function()
+            local root = vim.fn.getcwd()
+            local name = vim.fn.fnamemodify(root, ":t")
+            local hash = vim.fn.sha256(root):sub(1, 8) -- names are unique as well. check ~/.local/share/nvim/jdtls-workspaces/
+            return vim.fn.stdpath("data")
+            .. "/jdtls-workspaces/"
+            .. name
+            .. "_"
+            .. hash
+        end)(),
+    },
+
+    root_markers = {
+        "pom.xml",
+        "build.gradle",
+        "build.gradle.kts",
+        ".git",
+        "Makefile",
+    },
+})
+
 do
-	local luacheck = require("efmls-configs.linters.luacheck")
-	local stylua = require("efmls-configs.formatters.stylua")
+    local luacheck = require("efmls-configs.linters.luacheck")
+    local stylua = require("efmls-configs.formatters.stylua")
 
-	local flake8 = require("efmls-configs.linters.flake8")
-	local black = require("efmls-configs.formatters.black")
+    local flake8 = require("efmls-configs.linters.flake8")
+    local black = require("efmls-configs.formatters.black")
 
-	local prettier_d = require("efmls-configs.formatters.prettier_d")
-	local eslint_d = require("efmls-configs.linters.eslint_d")
+    local prettier_d = require("efmls-configs.formatters.prettier_d")
+    local eslint_d = require("efmls-configs.linters.eslint_d")
 
-	local fixjson = require("efmls-configs.formatters.fixjson")
+    local fixjson = require("efmls-configs.formatters.fixjson")
 
-	local shellcheck = require("efmls-configs.linters.shellcheck")
-	local shfmt = require("efmls-configs.formatters.shfmt")
+    local shellcheck = require("efmls-configs.linters.shellcheck")
+    local shfmt = require("efmls-configs.formatters.shfmt")
 
-	local cpplint = require("efmls-configs.linters.cpplint")
-	local clangfmt = require("efmls-configs.formatters.clang_format")
+    local cpplint = require("efmls-configs.linters.cpplint")
+    local clangfmt = require("efmls-configs.formatters.clang_format")
 
-	local go_revive = require("efmls-configs.linters.go_revive")
-	local gofumpt = require("efmls-configs.formatters.gofumpt")
+    local go_revive = require("efmls-configs.linters.go_revive")
+    local gofumpt = require("efmls-configs.formatters.gofumpt")
 
-	vim.lsp.config("efm", {
-		filetypes = {
-			-- "c",
-			-- "cpp",
-			"css",
-			-- "go",
-			-- "html",
-			"javascript",
-			"javascriptreact",
-			"json",
-			"jsonc",
-			-- "lua",
-			"markdown",
-			-- "python",
-			"sh",
-			"typescript",
-			"typescriptreact",
-			"vue",
-			"svelte",
-		},
-		init_options = { documentFormatting = true },
-		settings = {
-			languages = {
-				c = { clangfmt, cpplint },
-				go = { gofumpt, go_revive },
-				cpp = { clangfmt, cpplint },
-				css = { prettier_d },
-				html = { prettier_d },
-				javascript = { eslint_d, prettier_d },
-				javascriptreact = { eslint_d, prettier_d },
-				json = { eslint_d, fixjson },
-				jsonc = { eslint_d, fixjson },
-				lua = { luacheck, stylua },
-				markdown = { prettier_d },
-				python = { flake8, black },
-				sh = { shellcheck, shfmt },
-				typescript = { eslint_d, prettier_d },
-				typescriptreact = { eslint_d, prettier_d },
-				vue = { eslint_d, prettier_d },
-				svelte = { eslint_d, prettier_d },
-			},
-		},
-	})
+    vim.lsp.config("efm", {
+        filetypes = {
+            -- "c",
+            -- "cpp",
+            "css",
+            -- "go",
+            -- "html",
+            "javascript",
+            "javascriptreact",
+            "json",
+            "jsonc",
+            -- "lua",
+            "markdown",
+            -- "python",
+            "sh",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "svelte",
+        },
+        init_options = { documentFormatting = true },
+        settings = {
+            languages = {
+                c = { clangfmt, cpplint },
+                go = { gofumpt, go_revive },
+                cpp = { clangfmt, cpplint },
+                css = { prettier_d },
+                html = { prettier_d },
+                javascript = { eslint_d, prettier_d },
+                javascriptreact = { eslint_d, prettier_d },
+                json = { eslint_d, fixjson },
+                jsonc = { eslint_d, fixjson },
+                lua = { luacheck, stylua },
+                markdown = { prettier_d },
+                python = { flake8, black },
+                sh = { shellcheck, shfmt },
+                typescript = { eslint_d, prettier_d },
+                typescriptreact = { eslint_d, prettier_d },
+                vue = { eslint_d, prettier_d },
+                svelte = { eslint_d, prettier_d },
+            },
+        },
+    })
 end
 
 vim.lsp.enable({
-	"lua_ls",
-	"pyright",
-	"bashls",
-	"ts_ls",
-	"gopls",
-	"clangd",
-	"efm",
-	"html",
-	"cssls",
-    "kotlin_lsp"
+    "lua_ls",
+    "pyright",
+    "bashls",
+    "ts_ls",
+    "gopls",
+    "clangd",
+    "efm",
+    "html",
+    "cssls",
+    "kotlin_lsp",
+    "jdtls"
 })
 
 -- snippets
